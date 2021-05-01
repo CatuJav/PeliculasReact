7968cdd3443c4ad1514dd130cc8474420b25a8fa
Javier <jalcoser1996@gmail.com>
2021-05-01 15:21:13 -0500

    Diseño inicial de la pantalla de Details

--- src/screens/DetailScree.tsx
+++ src/screens/DetailScree.tsx
@@ -1,6 +1,6 @@
 import { StackScreenProps } from '@react-navigation/stack';
 import React from 'react'
-import { View, Text } from 'react-native';
+import { View, Text, Image, StyleSheet, useWindowDimensions, ScrollView } from 'react-native';
 //import { Movie } from '../interfaces/movieInterface';
 import { RootStackParams } from '../navigation/Navigation';
 
@@ -10,12 +10,59 @@ import { RootStackParams } from '../navigation/Navigation';
 interface Props extends StackScreenProps<RootStackParams,'DetailScreen'>{}
 export const DetailScreen = ({route}:Props) => {
     
+    const {height}=useWindowDimensions();
     //Trata los parametros como Movie
     const movie = route.params;
+
+    const uri=`https://image.tmdb.org/t/p/w500${movie.poster_path}`
+
     console.log(movie.title)
     return (
-        <View>
-            <Text>DetailScreen</Text>
+       <ScrollView>
+            <View style={{...styles.imageContainer, height:height*0.7}}>
+           <Image
+            source={{uri}}
+            style={styles.posterImage}
+           />
         </View>
+        <View style={styles.marginContainer}>
+            <Text style={styles.subTitle}>{movie.original_title}</Text>
+            <Text style={styles.title}>{movie.title}</Text>
+        </View>
+       </ScrollView>
     )
 }
+
+const styles = StyleSheet.create({
+    imageContainer:{
+        width:'100%',
+        shadowColor: "green",
+        shadowOffset: {
+	        width: 0,
+	        height: 10,
+        },
+        shadowOpacity: 0.24,
+        shadowRadius: 7,
+
+        elevation: 10,
+        borderBottomEndRadius:25,
+        borderBottomStartRadius:25,
+        overflow:'hidden'
+    },
+    posterImage:{
+        flex:1,
+
+    },
+    marginContainer:{
+        marginHorizontal:20,
+        marginTop:20
+    },
+    subTitle:{
+        fontSize:16,
+        opacity:0.5,
+    },
+    title:{
+        fontSize:20,
+        fontWeight:'bold',
+    }
+});
