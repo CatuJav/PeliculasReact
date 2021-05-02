ec0a114f606c24e2e8b26cbf231ae0220563622b
Javier <jalcoser1996@gmail.com>
2021-05-01 20:38:08 -0500

    Mas detalle de peliculas
    Se añade la historia y el presupuestp

--- src/components/MovieDetails.tsx
+++ src/components/MovieDetails.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { Text, View } from 'react-native';
+import currencyFormater from 'currency-formatter';
+
 import { MovieFull } from '../interfaces/movieInterface';
 import { Cast } from '../interfaces/creditsInterface';
 import Icon from 'react-native-vector-icons/Ionicons';
@@ -14,16 +16,24 @@ export const MovieDetails = ({movieFull,cast}:Props) => {
             {/**Detalles */}
             <View style={{marginHorizontal:20}}>
                 <View style={{flexDirection:'row'}}>
-                <Icon
-                    name="star-outline"
-                    color="grey"
-                    size={20}
-                />
-               <Text> {movieFull.vote_average}</Text>
-               <Text style={{marginLeft:5}}>
-                   -{movieFull.genres.map((g)=>g.name).join(', ')}
-               </Text>
+                        <Icon
+                            name="star-outline"
+                            color="grey"
+                            size={20}
+                        />
+                    <Text> {movieFull.vote_average}</Text>
+                    <Text style={{marginLeft:5}}>
+                        -{movieFull.genres.map((g)=>g.name).join(', ')}
+                    </Text>
                 </View>
+                {/**Historia */}
+                <Text style={{fontSize:23, marginTop:10, fontWeight:'bold'}}>Historia</Text>
+                <Text style={{fontSize:16, textAlign:'justify'}}>{movieFull.overview}</Text>
+                {/**Presupuesto */}
+                <Text style={{fontSize:23, marginTop:10, fontWeight:'bold'}}>
+                    Presupuesto</Text>
+                <Text style={{fontSize:16, textAlign:'justify'}}>
+                    {currencyFormater.format(movieFull.budget,{code:'USD'})}</Text>
             </View>
         </>
     )
